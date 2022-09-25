0fef11faa56a1a127201aa52569d9061b3695b63
Hericles <hericlesvitorio@gmail.com>
2022-09-25 01:17:00 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Calculadora
+
+![preview](./.github/preview.png)
+
+Projeto contruído a partir do layout da calculadora do iphone, tendo como intuito principal, o aprendizado.
+
+[🔗 Clique aqui para acessar](https://github.com/HericlesCosta/Calculadora.git/)
+
+
+## 🛠 Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+- Git e Github
+
+## Contato
+
+hericlesvitorio@gmail.com
---
+++ script.js
@@ -0,0 +1,137 @@
+const numbers = document.querySelectorAll('.numbers');
+const result = document.querySelector('.display span');
+const signs = document.querySelectorAll('.sign');
+const equal = document.querySelector('.equal');
+const percent = document.querySelector('.percent');
+const negative = document.querySelector('.soso');
+const clear = document.querySelector('.clear');
+const comma = document.querySelector('.comma');
+
+let firstValue = '';
+let isFirstValue = false;
+let secondValue = '';
+let isSecondValue = false;
+let sign = '';
+let resultValue = 0;
+let commaValue = ''
+
+for (let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener('click', (e) => {
+        let atr = e.target.getAttribute('value');
+        if (isFirstValue === false){
+            getFirstValue(atr)
+        }
+        if (isSecondValue === false){
+            getSecondValue(atr)
+        }
+    })
+}
+
+function getFirstValue(el) {
+    result.innerHTML = '';
+    firstValue += el;
+    result.innerHTML = firstValue;
+    firstValue = +firstValue;
+}
+
+function getSecondValue(el) {
+    if (firstValue != '' && sign != ''){
+        secondValue += el;
+        result.innerHTML = secondValue;
+        secondValue = +secondValue;
+    }
+}
+
+comma.addEventListener('click', (e) => {
+    commaValue = e.target.getAttribute('value');
+    if (firstValue != '' && secondValue == ''){
+        getCommaValue(commaValue)
+    }
+    if (secondValue != '' && firstValue != ''){
+        getCommaValue(commaValue)
+    }
+})
+
+function getCommaValue(el) {
+    if (firstValue != '' && secondValue == ''){
+        firstValue += el;
+        result.innerHTML = firstValue;
+    }
+    if (secondValue != '' && firstValue != ''){
+        secondValue += el;
+        result.innerHTML = secondValue;
+    }
+}
+
+function getSign() {
+    for (let i = 0; i < signs.length; i++){
+        signs[i].addEventListener('click', (e) => {
+            sign = e.target.getAttribute('value');
+            isFirstValue = true;
+        })
+    }
+}
+getSign()
+
+equal.addEventListener('click', () => {
+    result.innerHTML = '';
+    if (sign === '+'){
+        resultValue = firstValue + secondValue;
+    } else if (sign === '-'){
+        resultValue = firstValue - secondValue;
+    } else if (sign === 'x'){
+        resultValue = firstValue * secondValue;
+    } else if (sign === '/'){
+        resultValue = firstValue / secondValue;
+    }
+    result.innerHTML = resultValue;
+    firstValue = resultValue;
+    secondValue = '';
+
+    checkResultLength();
+})
+
+function checkResultLength() {
+    resultValue = JSON.stringify(resultValue);
+
+    if (resultValue.length >= 8){
+        resultValue = JSON.parse(resultValue);
+        result.innerHTML = resultValue.toFixed(5);
+    }
+}
+
+negative.addEventListener('click', () => {
+    result.innerHTML = '';
+    if (firstValue != ''){
+        resultValue = -firstValue;
+        firstValue = resultValue;
+    }
+    if (firstValue != '' && secondValue != '' && sign != ''){
+        resultValue = -resultValue;
+    }
+
+    result.innerHTML = resultValue;
+})
+
+percent.addEventListener('click', () => {
+    result.innerHTML = '';
+    if (firstValue != ''){
+        resultValue = firstValue / 100;
+        firstValue = resultValue;
+    }
+    if (firstValue != '' && secondValue != '' && sign != ''){
+        resultValue = resultValue / 100;
+    }
+
+    result.innerHTML = resultValue;
+})
+
+clear.addEventListener('click', () => {
+    result.innerHTML = 0;
+    firstValue = '';
+    isFirstValue = false;
+    secondValue = '';
+    isSecondValue = false;
+    sign = '';
+    resultValue = 0;
+})
